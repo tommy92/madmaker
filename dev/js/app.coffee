c840aa8b11f0a505dab5309210f24b5704c8041d
Tomasz Jaworski <tomasz.jaworski@anixe.pl>
2014-01-15 16:36:43 +0100

    Another pages customization || Fixed nav

--- dev/js/app.coffee
+++ dev/js/app.coffee
@@ -1,8 +1,22 @@
 $( ->
+	$rootEl = $('html, body')
+	$headerEl = $('#header')
+
 	$('#nav a').click () ->
-		$('html, body').animate(
-      scrollTop: $( $(this).attr('href') ).offset().top
-      500
-    )
-    return false
+		$rootEl.animate(
+			scrollTop: $( $(this).attr('href') ).offset().top - 90
+			500
+		)
+		return false
+
+	$(window).scroll () ->
+		if $(window).scrollTop() > 57
+			$headerEl.addClass('fixed-nav')
+		else
+			$headerEl.removeClass('fixed-nav')
+
+		if $(window).scrollTop() > 570
+			$headerEl.addClass('show-btn')
+		else
+			$headerEl.removeClass('show-btn')
 )
--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,25 @@
 (function() {
   $(function() {
-    return $('#nav a').click(function() {
-      $('html, body').animate({
-        scrollTop: $($(this).attr('href')).offset().top
+    var $headerEl, $rootEl;
+    $rootEl = $('html, body');
+    $headerEl = $('#header');
+    $('#nav a').click(function() {
+      $rootEl.animate({
+        scrollTop: $($(this).attr('href')).offset().top - 90
       }, 500);
       return false;
+    });
+    return $(window).scroll(function() {
+      if ($(window).scrollTop() > 57) {
+        $headerEl.addClass('fixed-nav');
+      } else {
+        $headerEl.removeClass('fixed-nav');
+      }
+      if ($(window).scrollTop() > 570) {
+        return $headerEl.addClass('show-btn');
+      } else {
+        return $headerEl.removeClass('show-btn');
+      }
     });
   });
 
